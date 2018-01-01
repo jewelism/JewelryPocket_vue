2d3103366c1ff7c8ef5eb51a9e74a75211ef9920
jewelism <boseokjung@gmail.com>
2018-01-01 22:56:59 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    
+    <Wallet v-if="currentMenu=='wallet'"/>
+    <Invest v-else-if="currentMenu=='invest'"/>
+    <Convert v-else-if="currentMenu=='convert'"/>
+    <Intro v-else-if="currentMenu=='dev'"/>
+    <Price v-else="currentMenu=='price'"/>
+
+    <h1>Current Menu : {{currentMenu}}</h1>
+    <FooterMenu :onPress="onPressMenuItem" id="footer"/>
+  </div>
+</template>
+
+<script>
+import Price from './components/Price/Price'
+import Wallet from './components/Wallet/Wallet'
+import Invest from './components/Invest/Invest'
+import Convert from './components/Convert/Convert'
+import Intro from './components/Intro/Intro'
+import FooterMenu from './components/FooterMenu/FooterMenu'
+
+export default {
+  name: 'app',
+  data (){
+    return {
+      currentMenu: 'price'
+    }
+  },
+  methods: {
+    onPressMenuItem : function(menu){
+      console.log(menu)
+      this.currentMenu=menu
+    }
+  },
+  components: {
+    Price,
+    Wallet,
+    Invest,
+    Convert,
+    Intro,
+    FooterMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+},
+#footer {
+  position:'absolute';
+  bottom:0;
+  width:100%;
+  height:70px;
+  background:#ccc;
+}
+</style>
---
+++ src/components/FooterMenu/FooterMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="onPress('price')">시세정보</button>
+    <button @click="onPress('wallet')">지갑</button>
+    <button @click="onPress('invest')">가상투자</button>
+    <button @click="onPress('convert')">모의환전</button>
+    <button @click="onPress('dev')">개발자</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterMenu',
+  props: ['onPress'],
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
